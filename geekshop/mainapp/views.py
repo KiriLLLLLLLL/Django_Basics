4ae04229034769f97c42658a4dd2a69e488a2180
KIRILLNAX <03-033@mail.ru>
2021-07-22 17:32:21 +0300

    add Lesson3

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
 
+from mainapp.models import Product, CategoryMenu
+
+
 def index(request):
 
     title = 'каталог'
 
-    links_menu = [
-        {'href': 'index', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
+    #
+
+    products = Product.objects.all()[:4]
+
+    category_menu = CategoryMenu.objects.all()
 
     context = {
         'title': title,
-        'links_menu': links_menu,
+        #'links_menu': links_menu,
+        'related_products': products,
+        'category_menu': category_menu
     }
 
     return render(request, 'mainapp/products.html', context)
